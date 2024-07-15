2392108408845d46c248070d1eac5eab44cf7fa9
HimashRajapaksha <rajapakshahimash@gmail.com>
2024-07-15 15:09:12 +0530

    commit finalise after review

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -149,8 +149,10 @@ export default function AdminDashboard() {
                   value={content}
                   onChange={(e) => setContent(e.target.value)}
                   rows={5}
+                  style={{ height: '250px' }}
                   className="mt-1 block w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
                   required
+                  
                 ></textarea>
               </div>
               <div>
